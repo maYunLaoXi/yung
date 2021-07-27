77ce92f961f511dc315747b92ed6f194d4b93703
maYunLaoXi <593871583@qq.com>
2021-07-27 14:12:51 +0800

    fix cli.pause()

--- lib/cli.ts
+++ lib/cli.ts
@@ -1,6 +1,7 @@
 import { cac } from 'cac'
 import { rewriteConfig } from './utils'
 import { runViteCmd } from './viteUtils'
+import chalk from 'chalk'
 
 
 const cli = cac('yung')
@@ -10,31 +11,55 @@ export interface ServerOptions {
 
 cli
   .option('--base <path>', `[string] public base path (default: ./)`)
+cli
+  .command('[root]')
+  .action(() => {
+    console.log(chalk.blue(`
+      this command no used
+      run:  yung dev to start devement mode
+    `))
+  })
 
 // dev
 cli
   .command('serve [page]')
   .alias('dev')
   .action(async (page: string, options: ServerOptions): Promise<void> => {
-    const yungConfPath = rewriteConfig({ page, base: options.base || './' })
-    runViteCmd({ cmd: 'serve', configPath: yungConfPath })
+    try {
+      const yungConfPath = rewriteConfig({ page, base: options.base || './' })
+      runViteCmd({ cmd: 'serve', configPath: yungConfPath })
+    } catch (e) {
+      console.log(chalk.red(`error during dev server:\n${e.stack}`))
+      process.exit(1)
+    }
   })
 
 // prod
 cli
   .command('build [...pages]')
   .action(async (pages: string[], options: ServerOptions): Promise<void> => {
-    const [page] = pages
-    const yungConfPath = rewriteConfig({ page, base: options.base || './' })
-    runViteCmd({ cmd: 'build', configPath: yungConfPath })
+    try {
+      const [page] = pages
+      const yungConfPath = rewriteConfig({ page, base: options.base || './' })
+      runViteCmd({ cmd: 'build', configPath: yungConfPath })
+    } catch(e) {
+      console.log(chalk.red(`error during build:\n${e.stack}`))
+      process.exit(1)
+    }
   })
 
 // preview
 cli
   .command('preview [page]')
   .action(async (page: string, option: ServerOptions): Promise<void> => {
-    const yungConfPath = rewriteConfig({ page, base: option.base || './' })
-    runViteCmd({ cmd: 'preview', configPath: yungConfPath })
+    try {
+      const yungConfPath = rewriteConfig({ page, base: option.base || './' })
+      runViteCmd({ cmd: 'preview', configPath: yungConfPath })
+    } catch(e) {
+      console.log(chalk.red(`error when starting preview server:\n${e.stack}`))
+      process.exit(1)
+    }
   })
 cli.help()
 cli.version(require('../package.json').version)
+cli.parse()
